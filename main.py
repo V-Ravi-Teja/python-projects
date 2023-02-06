5c759c1077d47df6fbfb693df52ab35d6066bb79
V-Ravi-Teja <raviteja.vobbilineni@gmail.com>
2023-02-06 21:26:08 +0530

    removed bugs

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import PySimpleGUI as sg
 
 import processes
 
+sg.theme('Topanga')
+
 prompt = sg.Text("Enter a To-Do")
 to_do = sg.InputText(key='todo')
 add_button = sg.Button('ADD', tooltip='click to add a To-Do')
@@ -34,20 +36,31 @@ while True:
             processes.write_todos(todos)
             box['todos'].update(values=todos)
         case 'EDIT':
-            todo_to_edit = value['todos'][0]
-            new_todo = value['todo']
-            todos = processes.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo + '\n'
-            processes.write_todos(todos)
-            box['todos'].update(values=todos)
+            try:
+                todo_to_edit = value['todos'][0]
+                new_todo = value['todo']
+                todos = processes.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo + '\n'
+                processes.write_todos(todos)
+                box['todos'].update(values=todos)
+            except IndexError:
+                sg.popup('Please select an item first',
+                         title='ERROR',
+                         font=('Helvetica', 15))
         case 'COMPLETE':
-            todo_that_is_completed = value['todos'][0]
-            todos = processes.get_todos()
-            index = todos.index(todo_that_is_completed)
-            todos.pop(index)
-            processes.write_todos(todos)
-            box['todos'].update(values=todos)
+            try:
+                todo_that_is_completed = value['todos'][0]
+                todos = processes.get_todos()
+                index = todos.index(todo_that_is_completed)
+                todos.pop(index)
+                processes.write_todos(todos)
+                box['todos'].update(values=todos)
+                box['todo'].update(value='')
+            except IndexError:
+                sg.popup('Please select an item first',
+                         title='ERROR',
+                         font=('Helvetica', 15))
         case 'todos':
             box['todo'].update(value=value['todos'][0])
         case 'CLOSE':
